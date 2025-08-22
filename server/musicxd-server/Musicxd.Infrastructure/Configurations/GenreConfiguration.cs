497773c2f96278172b91770c4beb2ea47460758e
Sathvika-jain <sajai@eg.dk>
2025-08-22 10:45:50 +0530

    written a configuration for Genre and Studio

---
+++ server/musicxd-server/Musicxd.Infrastructure/Configurations/GenreConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Musicxd.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musicxd.Infrastructure.Configurations
+{
+    internal class GenreConfiguration : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            builder.HasKey(g=>g.GenreId);
+
+            builder.Property(g => g.GenreId)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("genre_id");
+
+            builder.Property(g => g.GenreName)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnName("genre_name");
+
+            builder.HasMany(a => a.Albums)
+                .WithMany(g => g.Genres);
+               
+            builder.HasIndex(g => g.GenreName)
+              .IsUnique()
+              .HasDatabaseName("IX_Genre_GenreName");
+
+        }
+    }
+}
---
+++ server/musicxd-server/Musicxd.Infrastructure/Configurations/StudioConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Musicxd.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musicxd.Infrastructure.Configurations
+{
+    internal class StudioConfiguration : IEntityTypeConfiguration<Studio>
+    {
+        public void Configure(EntityTypeBuilder<Studio> builder)
+        {
+            builder.HasKey(s=>s.StudioId);
+
+            builder.Property(s => s.StudioId)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("studio_id");
+
+            builder.Property(s => s.StudioName)
+                .IsRequired()
+                .HasMaxLength(200)
+                .HasColumnName("studio_name");
+
+            
+            builder.HasMany(a=>a.Albums)
+                .WithOne(s=>s.Studio)
+                .HasForeignKey(s => s.StudioId) 
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(s => s.StudioName)
+               .IsUnique()
+               .HasDatabaseName("IX_Studio_StudioName");
+
+
+
+            builder.ToTable("studio");
+
+        }
+    }
+}
